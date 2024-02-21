9fc9c0e39554f42c05cf8bf1ff7e93b64606e7a3
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2024-02-21 15:18:16 +0900

    リストアイテムを選択して、フォームのプレースホルダーに表示する

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='MemoApp'>
+        <List/>
+      </div>
+  );
+}
+
+function List(){
+  const [showEdit, setShowEdit] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleLinkClick = (item) => {
+    setSelectedItem(item);
+    setShowEdit(true);
+  };
+
+  return (
+    <>
+      <ul className="display-memo-title">
+        <li><a href='#' onClick={() => handleLinkClick("foo")}>foo</a></li>
+        <li><a href='#' onClick={() => handleLinkClick("baa")}>bar</a></li>
+        <li><a href='#' onClick={() => handleLinkClick("buzz")}>buzz</a></li>
+      </ul>
+      <p><a href='#'>+</a></p>
+      {showEdit && <Edit selectedItem={selectedItem}/>}
+    </>
+  );
+}
+
+function Edit({selectedItem}){
+  return (
+    <div className="edit-memo">
+      <form>
+        <input type="text" name="name" id="name" value={selectedItem}/>
+        <input type="submit" value="編集" />
+        <input type="submit" value="削除" />
+      </form>
     </div>
   );
 }
