869e10417ba9e0a6b4f2921292f083de2a23d93a
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2024-05-13 15:21:22 +0900

    フォームに依存する処理を子コンポーネントで行う

--- src/App.jsx
+++ src/App.jsx
@@ -28,29 +28,9 @@ export default function App() {
     }
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const inputText = formData.get("memotext");
-    const title = inputText.split("\n")[0];
-    const text = inputText;
-
-    const existTitleMemo = memos.some((memo) => memo.text === text);
-    if (existTitleMemo) {
-      window.alert("全く同じ内容のメモが既に存在します。");
-    } else {
-      const newMemosForList = memos.map((memo) => {
-        if (memo.id === choosedMemo.id) {
-          return { id: memo.id, title: title, text: text };
-        } else {
-          return memo;
-        }
-      });
-
-      setMemos(newMemosForList);
-      localStorage.removeItem(choosedMemo.title);
-      localStorage.setItem(title, text);
-    }
+  const update = () => {
+    localStorage.removeItem(choosedMemo.title);
+    localStorage.setItem(choosedMemo.title, choosedMemo.text);
   };
 
   const destroy = () => {
@@ -74,8 +54,10 @@ export default function App() {
       <div className="sentence">
         {editable && (
           <Edit
+            memos={memos}
             choosedMemo={choosedMemo}
-            handleSubmit={handleSubmit}
+            setMemos={setMemos}
+            update={update}
             destroy={destroy}
           />
         )}
--- src/Edit.jsx
+++ src/Edit.jsx
@@ -1,12 +1,46 @@
 import TextArea from "./TextArea.jsx";
 
-export default function Edit({ choosedMemo, handleSubmit, destroy }) {
+export default function Edit({
+  memos,
+  choosedMemo,
+  setMemos,
+  update,
+  destroy,
+}) {
+  const handleFormat = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const inputText = formData.get("memotext");
+    const title = inputText.split("\n")[0];
+    const text = inputText;
+
+    const existTitleMemo = memos.some((memo) => memo.text === text);
+    if (existTitleMemo) {
+      window.alert("全く同じ内容のメモが既に存在します。");
+    } else {
+      const newMemosForList = memos.map((memo) => {
+        if (memo.id === choosedMemo.id) {
+          return { id: memo.id, title: title, text: text };
+        } else {
+          return memo;
+        }
+      });
+
+      setMemos(newMemosForList);
+    }
+  };
+
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleFormat}>
         <TextArea key={choosedMemo.id} inputText={choosedMemo.text} />
         <br />
-        <button type="submit" name="button" value="submit">
+        <button
+          type="submit"
+          name="button"
+          value="submit"
+          onClick={() => update()}
+        >
           編集
         </button>
         <button type="button" name="destroy" onClick={() => destroy()}>
